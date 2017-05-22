12ff6f68eb87e40048af4570d5df1d42b5aac5a5
Mike van Genderen <mikevg466>
2017-05-22 12:27:55 -0400

    Removes Dummy Data from Stations

--- browser/react/components/Stations.js
+++ browser/react/components/Stations.js
@@ -2,21 +2,16 @@ import React from 'react';
 import {Link} from 'react-router';
 
 export default function Stations (props) {
-	console.log("Stations props: ", props)
-	const DUMMY_STATIONS_DATA = [
-	  { name: '90s Hip Hop' },
-	  { name: 'Death Metal' },
-	  { name: 'Classical' }
-	];
-
+	console.log(props);
+	console.log(props.stations);
 	return (<div>
 		      <h3>Stations</h3>
 		      <div className="list-group">
 		      {
-		        DUMMY_STATIONS_DATA.map(station => {
+		        props.stations && Object.keys(props.stations).map(station => {
 		          return (
-		            <div className="list-group-item" key={station.name}>
-		              <Link to={'fill/me/in/later'}>{station.name}</Link>
+		            <div className="list-group-item" key={station}>
+		              <Link to={'fill/me/in/later'}>{station}</Link>
 		            </div>
 		          );
 		        })
